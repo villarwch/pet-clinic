49cf51aaf77124103699fbe779fb7553c6652b21
Daniel Villar <dan.villarwch@gmail.com>
2026-08-04 21:02:20 -0400

    probar statico xd

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -28,8 +28,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PetClinicApplication {
 
+	private static final String password = "Admin123!";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
 }
+
